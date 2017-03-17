38ba14169f0f6aa1087b1909c4a17fe5031cf44c
BM Services <support@bm-services.com>
2017-03-17 14:41:05 +0100

    [*] Order process: clean quote / order in customer checkout session on cancellation, payment error

--- Controller/Payment.php
+++ Controller/Payment.php
@@ -1,24 +1,35 @@
 <?php
-
 /**
  * Paybox Epayment module for Magento
  *
- * This source file is subject to the Open Software License (OSL 3.0)
- * available at : http://opensource.org/licenses/osl-3.0.php
+ * Feel free to contact Paybox by Verifone at support@paybox.com for any
+ * question.
  *
- * @package    Paybox_Epayment
- * @copyright  Copyright (c) 2013-2014 Paybox
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * LICENSE: This source file is subject to the version 3.0 of the Open
+ * Software License (OSL-3.0) that is available through the world-wide-web
+ * at the following URI: http://opensource.org/licenses/OSL-3.0. If
+ * you did not receive a copy of the OSL-3.0 license and are unable
+ * to obtain it through the web, please send a note to
+ * support@paybox.com so we can mail you a copy immediately.
+ *
+ *
+ * @version   1.0.6
+ * @author    BM Services <contact@bm-services.com>
+ * @copyright 2012-2017 Paybox
+ * @license   http://opensource.org/licenses/OSL-3.0
+ * @link      http://www.paybox.com/
  */
 
 namespace Paybox\Epayment\Controller;
 
-class Payment extends \Magento\Framework\App\Action\Action {
+class Payment extends \Magento\Framework\App\Action\Action
+{
 
     /**
      * @var \Magento\Framework\View\Result\PageFactory
      */
     protected $resultPageFactory;
+    protected $_quoteRepository;
     protected $_logger;
 
     /**
@@ -26,24 +37,62 @@ class Payment extends \Magento\Framework\App\Action\Action {
      * @param \Magento\Framework\View\Result\PageFactory resultPageFactory
      */
     public function __construct(
-    \Magento\Framework\App\Action\Context $context, \Magento\Framework\View\Result\PageFactory $resultPageFactory
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
     ) {
         $this->resultPageFactory = $resultPageFactory;
         parent::__construct($context);
 
         $this->_logger = $this->_objectManager->get('Psr\Log\LoggerInterface');
         $this->_messageManager = $this->_objectManager->get('Magento\Framework\Message\ManagerInterface');
+        $this->_quoteRepository = $this->_objectManager->create('Magento\Quote\Api\CartRepositoryInterface');
     }
 
-    public function execute() {
+    public function execute()
+    {
+    }
+
+    protected function _redirectResponse($order, $success, $checkUrlWarn = false)
+    {
+        // clear all messages in session
+        $this->messageManager->getMessages(true);
+
+        $storeId = $order->getStore()->getId();
         
+        if ($success) {
+            $this->_getCheckout()->setLastQuoteId($order->getQuoteId())
+                                ->setLastSuccessQuoteId($order->getQuoteId())
+                                ->setLastOrderId($order->getId())
+                                ->setLastRealOrderId($order->getIncrementId())
+                                ->setLastOrderStatus($order->getStatus());
+
+            $this->logDebug('Redirecting to success page.');
+            $this->_redirect('checkout/onepage/success');
+        } else {
+            $this->logDebug('Unsetting order data in session.');
+            $this->messageManager->addWarning(__('Checkout and order have been canceled.'));
+
+            $this->logDebug("Restore cart for order #{$order->getId()} to allow re-order quicker.");
+            $quote = $this->_quoteRepository->get($order->getQuoteId());
+            if ($quote->getId()) {
+                $quote->setIsActive(true)->setReservedOrderId(null);
+                $this->_quoteRepository->save($quote);
+
+                $this->_getCheckout()->replaceQuote($quote);
+            }
+
+            $this->logDebug('Redirecting to cart page.');
+            $this->_redirect('checkout/cart', array('_store' => $storeId));
+        }
     }
 
-    protected function _404() {
+    protected function _404()
+    {
         $this->_forward('defaultNoRoute');
     }
 
-    protected function _loadQuoteFromOrder(\Magento\Sales\Model\Order $order) {
+    protected function _loadQuoteFromOrder(\Magento\Sales\Model\Order $order)
+    {
         $quoteId = $order->getQuoteId();
 
         // Retrieves quote
@@ -56,7 +105,18 @@ class Payment extends \Magento\Framework\App\Action\Action {
         return $quote;
     }
 
-    protected function _getOrderFromParams(array $params) {
+    /**
+     * Get checkout session namespace.
+     *
+     * @return \Magento\Checkout\Model\Session
+     */
+    protected function _getCheckout()
+    {
+        return $this->_objectManager->get('Magento\Checkout\Model\Session');
+    }
+
+    protected function _getOrderFromParams(array $params)
+    {
         // Retrieves order
         $paybox = $this->getPaybox();
         $order = $paybox->untokenizeOrder($params['reference']);
@@ -66,32 +126,38 @@ class Payment extends \Magento\Framework\App\Action\Action {
         return $order;
     }
 
-    public function getConfig() {
+    public function getConfig()
+    {
         return $this->_objectManager->get('Paybox\Epayment\Model\Config');
     }
 
-    public function getPaybox() {
+    public function getPaybox()
+    {
         return $this->_objectManager->get('Paybox\Epayment\Model\Paybox');
     }
 
-    public function getSession() {
+    public function getSession()
+    {
         return $this->_objectManager->get('Magento\Checkout\Model\Session');
     }
 
-    public function logDebug($message) {
+    public function logDebug($message)
+    {
         $this->_logger->debug($message);
     }
 
-    public function logWarning($message) {
+    public function logWarning($message)
+    {
         $this->_logger->warning($message);
     }
 
-    public function logError($message) {
+    public function logError($message)
+    {
         $this->_logger->error($message);
     }
 
-    public function logFatal($message) {
+    public function logFatal($message)
+    {
         $this->_logger->critical($message);
     }
-
 }
--- Controller/Payment/Redirect.php
+++ Controller/Payment/Redirect.php
@@ -2,90 +2,123 @@
 /**
  * Paybox Epayment module for Magento
  *
- * This source file is subject to the Open Software License (OSL 3.0)
- * available at : http://opensource.org/licenses/osl-3.0.php
+ * Feel free to contact Paybox by Verifone at support@paybox.com for any
+ * question.
  *
- * @package    Paybox_Epayment
- * @copyright  Copyright (c) 2013-2014 Paybox
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * LICENSE: This source file is subject to the version 3.0 of the Open
+ * Software License (OSL-3.0) that is available through the world-wide-web
+ * at the following URI: http://opensource.org/licenses/OSL-3.0. If
+ * you did not receive a copy of the OSL-3.0 license and are unable
+ * to obtain it through the web, please send a note to
+ * support@paybox.com so we can mail you a copy immediately.
+ *
+ *
+ * @version   1.0.6
+ * @author    BM Services <contact@bm-services.com>
+ * @copyright 2012-2017 Paybox
+ * @license   http://opensource.org/licenses/OSL-3.0
+ * @link      http://www.paybox.com/
  */
 
 namespace Paybox\Epayment\Controller\Payment;
 
 class Redirect extends \Paybox\Epayment\Controller\Payment
 {
-	public function execute() {
-		$cookieName = 'lastOrderId';
-		$cookieManager = $this->_objectManager->get('Magento\Framework\Stdlib\CookieManagerInterface');
-		$encryptor = $this->_objectManager->get('Magento\Framework\Encryption\Encryptor');
-		$registry = $this->_objectManager->get('Magento\Framework\Registry');
+    public function execute()
+    {
+        $cookieName = 'lastOrderId';
+        $cookieManager = $this->_objectManager->get('Magento\Framework\Stdlib\CookieManagerInterface');
+        $encryptor = $this->_objectManager->get('Magento\Framework\Encryption\Encryptor');
+        $registry = $this->_objectManager->get('Magento\Framework\Registry');
 
-		// Retrieves order id
-		$session = $this->getSession();
-		$orderId = $session->getLastRealOrderId();
+        // Retrieves order id
+        $session = $this->getSession();
+        $orderId = $session->getLastRealOrderId();
 
-		// If none, try previously saved
-		$this->logDebug('Paybox - LastRealOrderId from $session: '.$orderId);
-		if (is_null($orderId)) {
-			$orderId = $session->getCurrentPbxepOrderId();
-			$this->logDebug('Paybox - CurrentPbxepOrderId from $session: '.$orderId);
-		}
+        // If none, try previously saved
+        $this->logDebug('Paybox - LastRealOrderId from $session: '.$orderId);
+        if (is_null($orderId)) {
+            $orderId = $session->getCurrentPbxepOrderId();
+            $this->logDebug('Paybox - CurrentPbxepOrderId from $session: '.$orderId);
+        }
 
-		//Try with cookies
-		$cookieOrderId = $cookieManager->getCookie($cookieName);
+        //Try with cookies
+        $cookieOrderId = $cookieManager->getCookie($cookieName);
 
-		// If none, 404
-		if (is_null($orderId)) {
-			$this->logDebug('Paybox - $orderId is null => 404');
+        // If none, 404
+        if (is_null($orderId)) {
+            $this->logDebug('Paybox - $orderId is null => 404');
 
-			$this->logDebug('Paybox - Try to get id from cookies');
-			if(!is_null($cookieOrderId)){
-				$this->logDebug('Paybox - Retrieve id from cookies : ' . $cookieOrderId);
-				$order = $this->_objectManager->get('Magento\Sales\Model\Order')->load($cookieOrderId);
-				if(isset($_COOKIE[$cookieName])){
-					unset($_COOKIE[$cookieName]);
-				}
-			}else{
-				return $this->_404();
-			}
-		}else{
-			$order = $this->_objectManager->get('Magento\Sales\Model\Order')->loadByIncrementId($orderId);
-		}
+            $this->logDebug('Paybox - Try to get id from cookies');
+            if (!is_null($cookieOrderId)) {
+                $this->logDebug('Paybox - Retrieve id from cookies : ' . $cookieOrderId);
+                $order = $this->_objectManager->get('Magento\Sales\Model\Order')->load($cookieOrderId);
+                if (isset($_COOKIE[$cookieName])) {
+                    unset($_COOKIE[$cookieName]);
+                }
+            } else {
+                return $this->_404();
+            }
+        } else {
+            $order = $this->_objectManager->get('Magento\Sales\Model\Order')->loadByIncrementId($orderId);
+        }
 
-		// Load order
-		$order = $this->_objectManager->get('Magento\Sales\Model\Order')->loadByIncrementId($orderId);
-		if (is_null($order) || is_null($order->getId())) {
-			$session->unsCurrentPbxepOrderId();
-			return $this->_404();
-		}
+        // Load order
+        $order = $this->_objectManager->get('Magento\Sales\Model\Order')->loadByIncrementId($orderId);
+        if (is_null($order) || is_null($order->getId())) {
+            $session->unsCurrentPbxepOrderId();
+            return $this->_404();
+        }
 
-		// Check order status
-		$state = $order->getState();
-		if ($state != \Magento\Sales\Model\Order::STATE_NEW) {
-			$session->unsCurrentPbxepOrderId();
-			return $this->_404();
-		}
+        // Check order status
+        $state = $order->getState();
+        if ($state != \Magento\Sales\Model\Order::STATE_NEW) {
+            $session->unsCurrentPbxepOrderId();
+            return $this->_404();
+        }
 
-		// Save id
-		$session->setCurrentPbxepOrderId($orderId);
+        // Save id
+        $session->setCurrentPbxepOrderId($orderId);
 
-		// Keep track of order for security check
-		$orders = $session->getPbxepOrders();
-		if (is_null($orders)) {
-			$orders = array();
-		}
+        // Keep track of order for security check
+        $orders = $session->getPbxepOrders();
+        if (is_null($orders)) {
+            $orders = array();
+        }
 
-		$orders[$encryptor->encrypt($orderId)] = true;
-		$session->setPbxepOrders($orders);
+        $orders[$encryptor->encrypt($orderId)] = true;
+        $session->setPbxepOrders($orders);
 
-		// Payment method
-		$order->getPayment()->getMethodInstance()->onPaymentRedirect($order);
+        // Payment method
+        $order->getPayment()->getMethodInstance()->onPaymentRedirect($order);
 
-		// Render form
-		$registry->register('pbxep/order_' . $orderId, $order);
+        // Render form
+        $registry->register('pbxep/order_' . $orderId, $order);
 
-		$page = $this->resultPageFactory->create();
-		
-		return $page;
-	}
+        $page = $this->resultPageFactory->create();
+
+        // check that there is products in cart
+        if ($order->getTotalDue() == 0) {
+            $this->logDebug('Paybox - Payment attempt with no amount : ' . $order->getId());
+            return $this->_404();
+        }
+
+        // check that order is not processed yet
+        if (!$this->_getCheckout()->getLastSuccessQuoteId()) {
+            $this->logDebug('Paybox - Payment attempt with a quote already processed : ' . $order->getId());
+            return $this->_404();
+        }
+
+        // add history comment and save it
+        $order->addStatusHistoryComment(__('Paybox - Client sent to Paybox payment page.'), false)
+                ->setIsCustomerNotified(false)
+                ->save();
+
+        // clear quote data
+        $this->_getCheckout()->unsLastQuoteId()
+                            ->unsLastSuccessQuoteId()
+                            ->clearHelperData();
+        
+        return $page;
+    }
 }
